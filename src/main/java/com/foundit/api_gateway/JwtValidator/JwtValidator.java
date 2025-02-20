eb6c1e41573bcc7cbf616fbbf207e29e289a0127
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-02-20 16:07:11 -0500

    Service registry implementations and Authentication validation

---
+++ src/main/java/com/foundit/api_gateway/JwtValidator/JwtValidator.java
@@ -0,0 +1,28 @@
+package com.foundit.api_gateway.JwtValidator;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtValidator {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
