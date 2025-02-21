af971ae91fae3b810be49eaddf4fa113079ecc38
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-02-21 15:24:30 -0500

    Authentication Gateway Changes

--- api-gateway/src/main/java/com/foundit/api_gateway/JwtValidator/JwtValidator.java
+++ api-gateway/src/main/java/com/foundit/api_gateway/JwtValidator/JwtValidator.java
@@ -1,14 +1,15 @@
 package com.foundit.api_gateway.JwtValidator;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
 @Component
+@PropertySource(value = {"classpath:application.yml"})
 public class JwtValidator {
 
     @Value("${security.jwt.secret-key}")
